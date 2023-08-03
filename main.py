c57e94a2a6253e8b96dcde8402e4b568a90f863c
Diya Patel <pateldiya703@gmail.com>
2023-08-03 22:29:04 +0530

    add new features to the visualization

--- main.py
+++ main.py
@@ -3,32 +3,48 @@ import plotly.express as px
 import numpy as np
 import pandas as pd
 
-st.set_page_config(layout='wide')
-data= pd.read_csv('./india.csv')
-list_of_states = list(data['State'].unique())
+st.set_page_config(layout='wide', page_title = 'Indian Census visualizer')
+data= pd.read_csv('./india3.csv')
+list_of_states = list(data['state'].unique())
 list_of_states.insert(0,'Overall India')
 
 st.sidebar.title('Visualizing India')
 selected_state= st.sidebar.selectbox('Select a state', list_of_states)
-primary = st.sidebar.selectbox('Select a primary parameter', sorted(data.columns[5:]))
-secondary = st.sidebar.selectbox('Select a secondary parameter', sorted(data.columns[5:]))
 
+parameter = st.sidebar.selectbox('Select a parameter', sorted(data.columns[6:]))
+# if 'Household' not in parameter:
+#     bubble = data['population']
+# else:
+#     bubble = data['households']
+if selected_state == 'Overall India':
+    filtered_data = data.copy()  # Use the entire dataset for overall India
+else:
+    filtered_data = data[data['state'] == selected_state]  # Filter data for the selected state
+
+if 'Household' not in parameter:
+    bubble = filtered_data['population']
+    color = filtered_data[parameter]
+else:
+    bubble = filtered_data['households']
+    color = filtered_data['households']
 plot = st.sidebar.button('Plot a graph')
 
 if plot:
-    st.text('Size of the bubble represents primary parameter')
-    st.text('Color of the bubble represents secondary parameter')
+    st.text('Size of the bubble represents population or number of households depending on the parameter')
+    st.text('Color of the bubble represents the parameter')
     if selected_state == 'Overall India':
         #map for india
-        fig = px.scatter_mapbox(data, lat='Latitude', lon='Longitude', size=primary, color=secondary,
+        fig = px.scatter_mapbox(data, lat='latitude', lon='longitude', size=bubble, color=parameter,
                                 size_max=35, height=600, width=1000,
-                                zoom=4, mapbox_style='carto-positron', hover_name='District'
+                                zoom=4, mapbox_style='carto-positron', hover_name='district'
                                 )
         st.plotly_chart(fig, size_container_width=True)
     else:
         # map for the state
-        state_df = data[data['State'] == selected_state]
-        fig = px.scatter_mapbox(state_df, lat='Latitude', lon='Longitude', size=primary, color=secondary,
+        state_df = data[data['state'] == selected_state]
+        fig = px.scatter_mapbox(state_df, lat='latitude', lon='longitude', size=bubble, color=parameter,
                                 size_max=35, height=600, width=1000,
-                                zoom=5, mapbox_style='carto-positron', hover_name='District'                                )
-        st.plotly_chart(fig, size_container_width=True)
+                                zoom=5, mapbox_style='carto-positron' , hover_name='district'
+                                )
+        st.plotly_chart(fig, use_container_width=True)
+        print(state_df)
